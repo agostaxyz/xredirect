43a8078845eed7d1548d25f8128b0e677a10a3dd
saga <agostaxyz@gmail.com>
2026-03-11 14:41:04 -0300

    add web-ext and polyfill

---
+++ .web-ext-config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    ignoreFiles: [
+        "node_modules",
+        "package.json",
+        "package-lock.json",
+        "pnpm-lock.yaml",
+        ".web-ext-config.js",
+        "*.md",
+        ".git",
+        ".gitignore",
+        "build"
+    ]
+};
