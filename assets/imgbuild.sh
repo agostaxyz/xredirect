5cef92d9804799344bd83d6fe8caac54b425e137
saga <agostaxyz@gmail.com>
2026-03-24 15:37:25 -0300

    img build script and new branding

--- assets/imgbuild.sh
+++ assets/imgbuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-magick -size 128x128 image.png icon-128.png
-magick -size 48x48 image.png icon-48.png
-magick -size 32x32 image.png icon-32.png
-magick icon-128.png  -modulate 100,0,100 off-128.png
+magick image.png -resize 128x128 -verbose icon-128.png
+magick image.png -resize 48x48 -verbose icon-48.png
+magick image.png -resize 32x32 -verbose icon-32.png
+magick icon-128.png  -verbose -modulate 100,0,100 off-128.png
